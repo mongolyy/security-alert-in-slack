de00957080ff88954e2d497a03d62dc2f824a2ed
mongolyy <mongolyy@gmail.com>
2022-12-28 09:07:08 +0900

    vulnerabilityを取得するように変更

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import core from '@actions/core'
+import github from '@actions/github'
+import {graphql} from '@octokit/graphql'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const {repo} = github.context
+    const hoge = await graphql(`
+      query {
+        repository(name: "${repo.repo}", owner: "${repo.owner}") {
+          vulnerabilityAlerts(first: 100, states: [OPEN]) {
+            nodes {
+              createdAt
+              dismissedAt
+              securityVulnerability {
+                package {
+                  name
+                }
+                advisory {
+                  description
+                }
+              }
+            }
+          }
+        }
+      }
+    `)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('vulnerabilityAlerts', hoge)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
